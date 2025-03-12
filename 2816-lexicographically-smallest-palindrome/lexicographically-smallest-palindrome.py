7a828293a7d99bf0c2b09d63d377acf09fc1984b
Asvitha-Vibeeshanan <166564611+Asvitha-Vibeeshanan@users.noreply.github.com>
2025-03-11 22:53:01 -0400

    Time: 37 ms (95.15%) | Memory: 18 MB (25.92%) - LeetSync

--- 2816-lexicographically-smallest-palindrome/lexicographically-smallest-palindrome.py
+++ 2816-lexicographically-smallest-palindrome/lexicographically-smallest-palindrome.py
@@ -6,18 +6,12 @@ class Solution:
         r = len(s) - 1
         s = list(s)
         while(l < r):
-            if s[l] == s[r]:
-                r = r-1
-                l = l+1
-            elif s[l] > s[r]:
-                #s = s.replace(s[l],s[r])
+            if s[l] > s[r]:
                 s[l] =s[r]
-                r = r-1
-                l = l+1
             else:
                 s[r] = s[l]
-                r = r-1
-                l = l+1
+            r = r-1
+            l = l+1
             
         return ''.join(s)
 
