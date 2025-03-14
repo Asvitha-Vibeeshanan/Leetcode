b211ceb89f2ec7a60d4f6b47260edce928ca4a7c
Asvitha-Vibeeshanan <166564611+Asvitha-Vibeeshanan@users.noreply.github.com>
2025-03-14 19:49:57 -0400

    Time: 24 ms (66.56%) | Memory: 35.3 MB (40.18%) - LeetSync

---
+++ 2519-find-the-original-array-of-prefix-xor/find-the-original-array-of-prefix-xor.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findArray(self, pref: List[int]) -> List[int]:
+        res = 0
+        arr = [0] * len(pref)
+        for i in range(len(pref)):
+            if i == 0:
+                arr[i] = pref[i]
+            else:
+                arr[i] = pref[i-1] ^ pref[i] 
+            
+            
+        return arr
+            
+        
