4dae2910931afed025d34c94b089d8a1c4de3df8
Asvitha-Vibeeshanan <166564611+Asvitha-Vibeeshanan@users.noreply.github.com>
2025-03-21 21:53:15 -0400

    Time: 7 ms (30.92%) | Memory: 18.3 MB (95.69%) - LeetSync

--- 2447-merge-similar-items/merge-similar-items.py
+++ 2447-merge-similar-items/merge-similar-items.py
@@ -1,16 +1,16 @@
 class Solution:
     def mergeSimilarItems(self, items1: List[List[int]], items2: List[List[int]]) -> List[List[int]]:
-        lst=[]
-        di={}
+        arr = []
+        dict = {}
         for i in items1:
-            if i[0] not in di:
-                di[i[0]]=i[1]
+            if i[0] not in dict:
+                dict[i[0]] = i[1]
         for i in items2:
-            if i[0] in di:
-                di[i[0]]+=i[1]
+            if i[0] in dict:
+                dict[i[0]] += i[1]
             else:
-                di[i[0]]=i[1]
-        for i,j in di.items():
-            lst.append([i,j])
-        lst.sort()
-        return lst
+                dict[i[0]] = i[1]
+        for i,j in dict.items():
+            arr.append([i,j])
+        arr.sort()
+        return arr
