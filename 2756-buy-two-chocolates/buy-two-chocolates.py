c1fe5c9a06998954e91f8614fdddc43835d16287
Asvitha-Vibeeshanan <166564611+Asvitha-Vibeeshanan@users.noreply.github.com>
2025-03-23 17:28:50 -0400

    Time: 0 ms (100.00%) | Memory: 17.7 MB (88.43%) - LeetSync

--- 2756-buy-two-chocolates/buy-two-chocolates.py
+++ 2756-buy-two-chocolates/buy-two-chocolates.py
@@ -1,11 +1,6 @@
 class Solution:
     def buyChoco(self, prices: List[int], money: int) -> int:
-        # Sort the prices to find the two cheapest chocolates
         prices.sort()
-        
-        # If the sum of the two cheapest chocolates is greater than the available money, return money
         if prices[0] + prices[1] > money:
             return money
-        
-        # Otherwise, subtract the cost of the two cheapest chocolates from the money
         return money - (prices[0] + prices[1])
