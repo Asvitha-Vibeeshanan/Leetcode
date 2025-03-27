a4ea2c5cc1cf8312f1c5399ff81b8c020e9fd0b5
Asvitha-Vibeeshanan <166564611+Asvitha-Vibeeshanan@users.noreply.github.com>
2025-03-27 18:16:10 -0400

    Time: 23 ms (54.14%) | Memory: 26.4 MB (74.05%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.py
+++ 219-contains-duplicate-ii/contains-duplicate-ii.py
@@ -11,31 +11,3 @@ class Solution:
             else:
                 return True
         return False
-        # if len(nums) == 1:
-        #     return False
-        # if len(nums) == set(nums):
-        #     return False
-        # # if len(nums) <= k :
-        # #     return True
-        
-        # dict = {}
-        # for i in range(len(nums)):
-        #     if nums[i] not in dict:
-        #         dict[nums[i]] = [i]
-        #     else:
-        #         dict[nums[i]].append(i)
-        # print(dict)
-
-
-        # for key, values in dict.items():
-        #     values = sorted(values, reverse = True)
-        #     if len(values) >=2:
-        #         if (values[0]-values[1]) <= k:
-        #             return True
-        #         else:
-        #             return False
-        #         # print(values)
-        #     #     if(dict[nums[i]] <= k):
-        #     #         return True
-        #     # return False
-        
