999c9176c21693b5ad15fa879add5061c5f7ede2
Asvitha Vibeeshanan <166564611+Asvitha-Vibeeshanan@users.noreply.github.com>
2025-04-02 16:44:04 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (30.58%) - LeetSync

--- 3447-clear-digits/clear-digits.py
+++ 3447-clear-digits/clear-digits.py
@@ -1,12 +1,9 @@
 class Solution:
     def clearDigits(self, s: str) -> str:
         stack = []
-        if s.isalpha():
-            return s
-        else:
-            for i in range(len(s)):
-                if s[i].isalpha():
-                    stack.append(s[i])
-                else:
-                    stack.pop()
+        for char in s:
+            if char.isalpha():
+                stack.append(char)
+            else:
+                stack.pop()
         return "".join(stack)              
