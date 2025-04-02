c7a15c260e94cc0993555c86d66194e23d35909d
Asvitha Vibeeshanan <166564611+Asvitha-Vibeeshanan@users.noreply.github.com>
2025-04-01 21:07:24 -0400

    Time: 16 ms (5.09%) | Memory: 17.6 MB (86.06%) - LeetSync

---
+++ 2886-faulty-keyboard/faulty-keyboard.py
@@ -0,0 +1,11 @@
+class Solution:
+    def finalString(self, s: str) -> str:
+        i = 0
+        while(s.find("i") > 0):
+            i = s.find("i")
+            sub = s[:i]
+            s = sub[::-1] + s[i+1:] 
+            #     i = i -1
+            # else:
+            #     i = i + 1
+        return s
