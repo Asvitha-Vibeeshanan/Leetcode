a75d0225c3caebe9a2b36747caa9bfb509b9cfd3
Asvitha Vibeeshanan <166564611+Asvitha-Vibeeshanan@users.noreply.github.com>
2025-04-01 21:56:34 -0400

    Time: 0 ms (100.00%) | Memory: 17.9 MB (16.89%) - LeetSync

---
+++ 3371-harshad-number/harshad-number.py
@@ -0,0 +1,13 @@
+class Solution:
+    def sumOfTheDigitsOfHarshadNumber(self, x: int) -> int:
+        sum = 0
+        temp = x
+        while(x):
+            digit = x%10
+            x = x//10
+            sum = sum + digit
+        if temp%sum == 0:
+            return sum
+        else:
+            return -1 
+        
