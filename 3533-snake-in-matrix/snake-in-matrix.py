0016d384d4db94386d911295846aa3a93d236981
Asvitha Vibeeshanan <166564611+Asvitha-Vibeeshanan@users.noreply.github.com>
2025-04-07 20:43:27 -0400

    Time: 4 ms (21.78%) | Memory: 17.8 MB (49.44%) - LeetSync

--- 3533-snake-in-matrix/snake-in-matrix.py
+++ 3533-snake-in-matrix/snake-in-matrix.py
@@ -1,14 +1,5 @@
 class Solution:
     def finalPositionOfSnake(self, n: int, commands: List[str]) -> int:
-        matrix = []
-
-        num = 0
-        for i in range(n):
-            row = []
-            for j in range(n):
-                row.append(num)
-                num += 1
-            matrix.append(row)
         i = 0
         j = 0
         for command in commands:
@@ -20,6 +11,5 @@ class Solution:
                 i = i - 1
             else:
                 i = i+1
-            current_position = matrix[i][j]
+            current_position = i*n + j
         return current_position
-
