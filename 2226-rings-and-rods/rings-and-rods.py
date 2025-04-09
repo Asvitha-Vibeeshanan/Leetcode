9a27eeb270393c5ca3974e1c7d902962258dfc00
Asvitha Vibeeshanan <166564611+Asvitha-Vibeeshanan@users.noreply.github.com>
2025-04-08 23:15:19 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (53.58%) - LeetSync

--- 2226-rings-and-rods/rings-and-rods.py
+++ 2226-rings-and-rods/rings-and-rods.py
@@ -1,30 +1,14 @@
 class Solution:
     def countPoints(self, rings: str) -> int:
-        # dict1 = {}
-        # for i in range(1, len(rings), 2):
-        #     rod = rings[i]
-        #     color = rings[i-1]
-        #     if rod not in dict1:
-        #         dict1[rod] = set()
-        #     dict1[rod].add(color)
-        
-        # count = 0
-        # for colors in dict1.values():
-        #     if {"B", "G", "R"}.issubset(colors):
-        #         count += 1
-        
-        # return count
-
         dict1 = {}
         rings = list(rings)
         for i in range(1,len(rings),2):
             if rings[i] not in dict1:
-                dict1[rings[i]] = {rings[i-1]}
+                dict1[rings[i]] = [rings[i-1]]
             else:
-                dict1[rings[i]].add(rings[i-1])
+                dict1[rings[i]].append(rings[i-1])
         count = 0
-        for colors in dict1.values():
-            if {"B", "G", "R"}.issubset(colors):
-                count += 1
-        
+        for key, value in dict1.items():
+            if "B" in value and "G" in value and "R" in value:
+                count = count + 1
         return count
