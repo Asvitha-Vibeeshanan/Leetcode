721f4cecfb6441b6f34ba50daf28913eea895248
Asvitha Vibeeshanan <166564611+Asvitha-Vibeeshanan@users.noreply.github.com>
2025-04-12 20:41:09 -0400

    Time: 4 ms (29.68%) | Memory: 18 MB (26.16%) - LeetSync

--- 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.py
+++ 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.py
@@ -1,14 +1,15 @@
 class Solution:
     def halvesAreAlike(self, s: str) -> bool:
+        arr = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
         count1 = 0
         count2 = 0
         n = len(s)//2
         string1 = s[:n]
         string2 = s[n:]
-        for i in range(len(string1)):
-            if string1[i].lower() == "a" or string1[i].lower() == "e" or string1[i].lower() == "i" or string1[i].lower() == "o" or string1[i].lower() == "u":
+        for i in range(n):
+            if string1[i] in arr:
                 count1 = count1 + 1
-            if string2[i].lower() == "a" or string2[i].lower() == "e" or string2[i].lower() == "i" or string2[i].lower() == "o" or string2[i].lower() == "u":
+            if string2[i] in arr:
                 count2 = count2 + 1
         
         return count1 == count2
