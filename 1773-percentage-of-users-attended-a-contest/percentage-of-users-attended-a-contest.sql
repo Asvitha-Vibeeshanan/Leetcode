f5d0ade7321f9460e4df7dd2322758fd7eebb8d7
Asvitha Vibeeshanan <166564611+Asvitha-Vibeeshanan@users.noreply.github.com>
2025-06-29 20:52:03 -0400

    Time: 845 ms (97.01%) | Memory: 0B (100.00%) - LeetSync

--- 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
+++ 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
@@ -5,10 +5,3 @@ select
 from register
 group by contest_id
 order by percentage desc, contest_id ASC;
-
--- SELECT 
---   r.contest_id,
---   ROUND(COUNT(DISTINCT r.user_id) * 100.0 / (SELECT COUNT(*) FROM Users), 2) AS percentage
--- FROM Register r
--- GROUP BY r.contest_id
--- ORDER BY r.contest_id;
