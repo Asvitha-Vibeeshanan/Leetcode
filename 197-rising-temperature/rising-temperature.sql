e13fb7aec4e7a1ccadd6ccfab1e5e48f18698569
Asvitha Vibeeshanan <166564611+Asvitha-Vibeeshanan@users.noreply.github.com>
2025-07-12 10:13:35 -0400

    Time: 377 ms (90.34%) | Memory: 0B (100.00%) - LeetSync

---
+++ 197-rising-temperature/rising-temperature.sql
@@ -0,0 +1,13 @@
+select id
+from (
+    select
+        id,
+        recordDate,
+        temperature,
+        lag(temperature) over (order by recordDate) as prev_temp,
+        lag(recordDate) over (order by recordDate) as prev_date
+    from Weather
+) as temp_diff
+where
+    Datediff(recordDate, prev_date) = 1
+    and temperature > prev_temp;
