287844c875b1099fdad45f8a332755676f0a724e
Asvitha Vibeeshanan <166564611+Asvitha-Vibeeshanan@users.noreply.github.com>
2025-09-15 10:42:35 -0400

    Time: 260 ms (92.32%) | Memory: 0B (100.00%) - LeetSync

--- 3816-dna-pattern-recognition/dna-pattern-recognition.sql
+++ 3816-dna-pattern-recognition/dna-pattern-recognition.sql
@@ -1,10 +1,9 @@
-select 
-    sample_id,  
-    dna_sequence, 
-    species,
-    case when dna_sequence like "ATG%" then 1 else 0 end as has_start,
-    case when dna_sequence like "%TAA" or  dna_sequence like "%TAG" or  dna_sequence like "%TGA" then 1 else 0 end as has_stop,
-    case when dna_sequence like "%ATAT%" then 1 else 0 end as has_atat,
-    case when dna_sequence REGEXP 'G{3,}' then 1 else 0 end as has_ggg
+select sample_id,
+       dna_sequence,
+       species,
+       case when dna_sequence like 'ATG%' then 1 else 0 end as has_start,
+       case when dna_sequence like '%TAA' or dna_sequence like '%TAG' or dna_sequence like '%TGA' then 1 else 0 end as has_stop,
+       case when dna_sequence like '%ATAT%' then 1 else 0 end as has_atat,
+       case when dna_sequence regexp 'G{3}' then 1 else 0 end as has_ggg
 from samples
 order by sample_id asc
