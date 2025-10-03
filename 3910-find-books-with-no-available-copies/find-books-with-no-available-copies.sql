afeedc38957962252010035fde1537be67c97a0f
Asvitha Vibeeshanan <166564611+Asvitha-Vibeeshanan@users.noreply.github.com>
2025-10-03 14:43:00 -0400

    Time: 533 ms (45.44%) | Memory: 0B (100.00%) - LeetSync

--- 3910-find-books-with-no-available-copies/find-books-with-no-available-copies.sql
+++ 3910-find-books-with-no-available-copies/find-books-with-no-available-copies.sql
@@ -1,8 +1,23 @@
 # Write your MySQL query statement below
-select l.book_id, l.title, l.author, l.genre, l.publication_year, count(b.book_id) as current_borrowers 
+select l.book_id,
+       l.title,
+       l.author,
+       l.genre,
+       l.publication_year,
+       count(l.book_id) as current_borrowers
 from library_books l
 join borrowing_records b on l.book_id = b.book_id
 where b.return_date is null
 group by l.book_id, l.title, l.author, l.genre, l.publication_year, l.total_copies
 having count(*) = l.total_copies
-order by current_borrowers desc, l.title asc;
+order by current_borrowers desc, l.title asc
+
+
+-- select l.book_id, l.title, l.author, l.genre, l.publication_year, count(b.book_id) as current_borrowers 
+-- from library_books l
+-- join borrowing_records b on l.book_id = b.book_id
+-- where b.return_date is null
+-- group by l.book_id, l.title, l.author, l.genre, l.publication_year, l.total_copies
+-- having count(*) = l.total_copies
+-- order by current_borrowers desc, l.title asc;
+ 
