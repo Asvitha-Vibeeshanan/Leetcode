7d30ad9bb377b808aab939b576596cde3b041595
Asvitha Vibeeshanan <166564611+Asvitha-Vibeeshanan@users.noreply.github.com>
2025-10-04 18:47:37 -0400

    Time: 1003 ms (94.93%) | Memory: 0B (100.00%) - LeetSync

--- 1174-sales-analysis-iii/sales-analysis-iii.sql
+++ 1174-sales-analysis-iii/sales-analysis-iii.sql
@@ -1,8 +1,6 @@
 # Write your MySQL query statement below
-select p.product_id, p.product_name 
+select p.product_id, p.product_name
 from Product p
-where p.product_id in (
-    select s.product_id 
-    from sales s 
-    group by product_id
-    having min(s.sale_date) >= '2019-01-01' and max(s.sale_date) <= '2019-03-31')
+where p.product_id in (select s.product_id from sales s
+group by s.product_id
+having min(s.sale_date)>='2019-01-01' and max(s.sale_date)<='2019-03-31')
