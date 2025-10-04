5a2318f81d6e9dbf72e13abbef2165a6d84f2945
Asvitha Vibeeshanan <166564611+Asvitha-Vibeeshanan@users.noreply.github.com>
2025-10-03 23:18:27 -0400

    Time: 318 ms (76.62%) | Memory: 0B (100.00%) - LeetSync

--- 2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
+++ 2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
@@ -1,5 +1,4 @@
-select employee_id
+select employee_id 
 from Employees
-where salary < 30000 
-  and manager_id not in (select employee_id from Employees)
+where salary < '30000' and manager_id not in (select employee_id from employees)
 order by employee_id
