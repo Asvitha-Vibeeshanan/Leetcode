343389db011e42b2a077d66fade07918e68f1e45
Asvitha Vibeeshanan <166564611+Asvitha-Vibeeshanan@users.noreply.github.com>
2025-10-07 09:14:38 -0400

    Time: 272 ms (94.58%) | Memory: 0B (100.00%) - LeetSync

--- 3848-analyze-subscription-conversion/analyze-subscription-conversion.sql
+++ 3848-analyze-subscription-conversion/analyze-subscription-conversion.sql
@@ -1,21 +1,7 @@
 # Write your MySQL query statement below
-select user_id, 
-round(sum(case when activity_type = 'free_trial' then activity_duration else 0 end)/ 
-NULLIF(SUM(CASE WHEN activity_type = 'free_trial' THEN 1 ELSE 0 END), 0),2) 
- AS trial_avg_duration, 
-round(sum(case when activity_type = 'paid' then activity_duration else 0 end)/ 
- NULLIF(SUM(CASE WHEN activity_type = 'paid' THEN 1 ELSE 0 END), 0),2)
- AS paid_avg_duration
-FROM UserActivity
-WHERE user_id IN (
-    SELECT DISTINCT user_id
-    FROM UserActivity
-    WHERE activity_type = 'free_trial'
-)
-AND user_id IN (
-    SELECT DISTINCT user_id
-    FROM UserActivity
-    WHERE activity_type = 'paid'
-)
-GROUP BY user_id
-ORDER BY user_id;
+select user_id, round(sum(case when activity_type ='free_trial' then activity_duration else 0 end)/nullif(count(CASE WHEN activity_type = 'free_trial' THEN 1 END), 0), 2) as trial_avg_duration,
+round(sum(case when activity_type ='paid' then activity_duration else 0 end)/nullif(count(CASE WHEN activity_type = 'paid' THEN 1 END), 0), 2) as paid_avg_duration
+from UserActivity
+where user_id in (select distinct user_id from UserActivity where activity_type = "free_trial") and user_id in (select distinct user_id from UserActivity where activity_type = "paid")
+group by user_id
+order by user_id
