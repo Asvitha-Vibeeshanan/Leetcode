43572130873f725c4ca401e99d9bb416d3e58d1b
Asvitha Vibeeshanan <166564611+Asvitha-Vibeeshanan@users.noreply.github.com>
2025-11-05 12:40:53 -0500

    Time: 900 ms (37.15%) | Memory: 0B (100.00%) - LeetSync

--- 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -1,4 +1,10 @@
 # Write your MySQL query statement below
-SELECT person_name from (select person_name,turn,
-sum(weight) over (order by turn) as cum from queue) p1
-where cum<=1000 order by turn desc limit 1;
+with cte as (select turn, person_id, person_name, weight, sum(weight) over (order by turn) as total_weight
+from Queue)
+
+select person_name from cte where total_weight <= 1000
+order by total_weight desc limit 1
+
+-- SELECT person_name from (select person_name,turn,
+-- sum(weight) over (order by turn) as cum from queue) p1
+-- where cum<=1000 order by turn desc limit 1;
